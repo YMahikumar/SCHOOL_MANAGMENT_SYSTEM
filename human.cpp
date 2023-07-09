bb36f3508f67ab2abf4b052eb4aa1d2a9f935ea4
YMahikumar <136109099+YMahikumar@users.noreply.github.com>
2023-07-09 21:56:54 +0530

    Add files via upload

---
+++ human.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class human
+{
+public:
+    string name;
+    int age;
+    long long phone;
+    string password;
+};
+
+
+
+
+
+// int main()
+// {
+//     AdmissionSystem admission;
+//     admission.addStudent();
+//     admission.displayStudents();
+//     admission.edit_details();
+//     admission.displayStudents();
+
+
+//     return 0;
+// }
